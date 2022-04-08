5227620a1a49d2584f43116a8d1b550cefce048e
Gift-Zw <giftmugaragumbojr@gmail.com>
2022-04-09 01:08:18 +0200

    updated allowed hosts

--- cybertechzw/settings.py
+++ cybertechzw/settings.py
@@ -11,9 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
-import dj_database_url
-from decouple import config
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -28,7 +26,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 SECRET_KEY = 'hw6u6_au@+dnxc5^qjq)3ce*ygh8lg)(inpr+fe&bih3*orf3e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['giftmugaragumbo.herokuapp.com']
 
@@ -83,7 +81,9 @@ WSGI_APPLICATION = 'cybertechzw.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-
+import django_heroku
+import dj_database_url
+from decouple import config
 DATABASES = {
     'default': dj_database_url.config(
         default=config('DATABASE_URL')
